9c8da11db760479e7e128a1b5906ea6806f714cd
Sergio Tz <sergiotz@gmail.com>
2023-08-13 20:50:38 -0300

    first commit

---
+++ src/main/java/com/example/springcloudgatewayrestgrpc/SpringCloudGatewayRestGrpcApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudgatewayrestgrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewayRestGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayRestGrpcApplication.class, args);
+	}
+
+}
